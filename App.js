e03979b14e4abefd83d20a88d497d959e535e5c0
Andy <anlaics2@gmail.com>
2018-04-30 10:44:28 -0700

    working sign up/sign in with otp

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 import SignUpForm from './components/SignUpForm';
 import SignInForm from './components/SignInForm';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    const config = {
+      apiKey: "AIzaSyDtrBXFwLWx-hVClCCQsomXp472hUyxHvk",
+      authDomain: "one-time-password-6f767.firebaseapp.com",
+      databaseURL: "https://one-time-password-6f767.firebaseio.com",
+      projectId: "one-time-password-6f767",
+      storageBucket: "one-time-password-6f767.appspot.com",
+      messagingSenderId: "604590465997"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -19,6 +32,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });

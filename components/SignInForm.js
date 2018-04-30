a89b233df3903eac30ba6e0d2277b3c2a017a768
Andy <anlaics2@gmail.com>
2018-04-30 10:24:08 -0700

    added sign in page with verify otp function

--- components/SignInForm.js
+++ components/SignInForm.js
@@ -5,21 +5,19 @@ import axios from 'axios';
 
 const ROOT_URL = 'https://us-central1-one-time-password-6f767.cloudfunctions.net';
 
-class SignUpForm extends Component {
-    state = { phone: '' };
+class SignInForm extends Component {
+    state = { phone: '', code: '' };
 
     handleSubmit = async () => {
-        // Create User w/ Phone Number
-        // Second Request runs even if first request was failed
-        // With aync/await, requests are only ran if previous requests finished
         try {
-            await axios.post(`${ROOT_URL}/createUser`, { phone: this.state.phone })
-            // Send OTP Code to Phone Number
-            await axios.post(`${ROOT_URL}/requestOneTimePassword`, { phone: this.state.phone })
+            axios.post(`${ROOT_URL}/verifyOneTimePassword`, {
+                phone: this.state.phone, code: this.state.code
+            })
         } catch (err) {
             console.log(err);
         }
     }
+
 
     render() {
         return (
@@ -31,10 +29,18 @@ class SignUpForm extends Component {
                         onChangeText={phone => this.setState({ phone })}
                     />
                 </View>
+
+                <View style={{ marginBottom: 10 }}>
+                    <FormLabel>Enter Code</FormLabel>
+                    <FormInput
+                        value={this.state.code}
+                        onChangeText={code => this.setState({ code })}
+                    />
+                </View>
                 <Button onPress={this.handleSubmit} title="Submit" />
             </View>
         )
     }
 }
 
-export default SignUpForm;
+export default SignInForm;
